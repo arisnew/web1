8c7a8bede85d8a367d9fc389100f93a0007f9da0
arisnew <aris.new2011@gmail.com>
2024-09-24 19:32:39 +0700

    format

--- inc/connect.php
+++ inc/connect.php
@@ -8,5 +8,5 @@ $conn = mysqli_connect($servername, $username, $password);
 
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
--- inc/database.php
+++ inc/database.php
@@ -1,31 +1,33 @@
 <?php
-class database {
- 
+class database
+{
+
     private $host = "127.0.0.1";
     private $uname = "root";
     private $pass = "root";
     private $db = "webdb1";
     public $con;
 
- function __construct(){
-    $this->con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
-    if (mysqli_connect_error()) {
-        die("error");
-    } else {
-        return $this->con;
+    function __construct()
+    {
+        $this->con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
+        if (mysqli_connect_error()) {
+            die("error");
+        } else {
+            return $this->con;
+        }
     }
- }
 
- function tampil_data(){
-	$data = $this->con->query("select * from users");
-    $hasil = [];
-	while($d = $data->fetch_assoc()){
-		$hasil[] = $d;
-	}
-	return $hasil;
+    function tampil_data()
+    {
+        $data = $this->con->query("select * from users");
+        $hasil = [];
+        while ($d = $data->fetch_assoc()) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
 }
-
-} 
 
 
 // di coba disini
